a959dab23fc8ce87e19f82ebb821e5407c61f077
GabrieleFrenkCode <80312004+GabrieleFrenkCode@users.noreply.github.com>
2021-03-09 09:20:48 +0100

    Add files via upload

---
+++ 6 - FUNCTION - OPEN_JSON WITH BULK LOAD.sql
@@ -0,0 +1,55 @@
+USE [JSON_DEMO]
+GO
+
+SET NOCOUNT ON
+DECLARE @SQL nvarchar(max)
+
+DECLARE @VALUE varchar(10) = 'Gabriele_1'
+
+SELECT @SQL = '
+SELECT J.* 
+FROM 
+	OPENROWSET(BULK N''YOUR_PATH\'+@VALUE+'.json'', SINGLE_CLOB) T
+	CROSS APPLY OPENJSON(T.BulkColumn) WITH 
+	(
+			Nome	           varchar(100)
+			,Cognome	       varchar(100)
+			,DataNascita       varchar(100)
+			,ComuneNascita     varchar(100)
+			,ProvinciaNascita  varchar(100)
+			,RegioneNascita    varchar(100)
+			,CAP               char(5)
+			,DescrizioneComune varchar(500)
+	) AS J'
+
+EXEC sp_executesql @SQL WITH RESULT SETS
+( 
+	(
+		Nome	           varchar(100)
+		,Cognome	       varchar(100)
+		,DataNascita       varchar(100)
+		,ComuneNascita     varchar(100)
+		,ProvinciaNascita  varchar(100)
+		,RegioneNascita    varchar(100)
+		,CAP               char(5)
+		,DescrizioneComune varchar(500)
+	)
+)
+
+SET NOCOUNT ON
+DECLARE @SQL nvarchar(max)
+
+DECLARE @VALUE varchar(100) = 'Gabriele_1'
+
+SELECT @SQL = '
+SELECT J.[Value] 
+FROM 
+	OPENROWSET(BULK N''YOUR_PATH\'+@VALUE+'.json'', SINGLE_CLOB) T
+	CROSS APPLY OPENJSON(T.BulkColumn) J '
+
+EXEC sp_executesql @SQL WITH RESULT SETS
+( 
+	(
+		[JSON_DATA] varchar(4000) NOT NULL
+	)
+)
